2283d15e865eba7be44d43f07f252a7e38681e52
Pantelis Magos <pmagos@gmail.com>
2017-02-08 14:45:44 +0200

    initial commit by Pas on Patmos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'ch-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ch works!';
+
+  items : FirebaseListObservable<any[]>;
+
+  constructor(private af: AngularFire) {}
+
+  ngOnInit() {
+    this.items = this.af.database.list('/SUN/chorus');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDzNkA864bX3HQ-FRLQq-HhMtyIYsI4HzE",
+    authDomain: "choruspool-5b2e8.firebaseapp.com",
+    databaseURL: "https://choruspool-5b2e8.firebaseio.com",
+    storageBucket: "choruspool-5b2e8.appspot.com",
+    messagingSenderId: "1032090914042"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
