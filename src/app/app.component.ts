357dfcb7fadc406f1651007c291f9b7e4d609265
Pantelis Magos <pmagos@gmail.com>
2017-02-08 17:34:13 +0200

    third commit, list found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'ch-root',
@@ -10,12 +11,18 @@ export class AppComponent implements OnInit {
   title = 'ch works!';
 
   choruses : FirebaseListObservable<any[]>;
+  chors: any[];
 
   constructor(private af: AngularFire) {}
 
   ngOnInit() {
     this.choruses = this.af.database.list('/SUN/chorus');
-    console.log(this.choruses);
+    this.choruses.subscribe(x => {
+      this.chors = x.map(function(a) {
+        return {name: a.name, comments: a.comments};
+      });
+      console.log(this.chors);
+    })
   }
 
   postChorusSample() {
